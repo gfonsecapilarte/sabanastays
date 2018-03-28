1f659af9643a0cc80361b54ca2b1d61226a16bc3
Andrey López <andreyslopez@gmail.com>
2018-03-27 19:41:25 -0500

    Revisión 37 [*] FIX: Error en el home module controller

--- app/Http/Controllers/Module/HomeController.php
+++ app/Http/Controllers/Module/HomeController.php
@@ -13,10 +13,14 @@ class HomeController extends Controller
         $result = HomeModel::with('lang')->first();
         $module = json_decode($result);
         HomeModel::parseLang($module);
+        $module->media_logo = null;
+        if (!is_null($module->id_media)) {
+            $module->media_logo = MediaModel::where('id_media', '=', $module->id_media)->with('lang')->get();
+        }
         //search media images
         $media_result = MediaModel::getMediaByType($module->id_home_module, 'home_module');
-        $module->sliders = json_decode($media_result);
-        MediaModel::parseLang($module->sliders);
+        $module->media = json_decode($media_result);
+        MediaModel::parseLang($module->media);
         return json_encode($module);
     }
 }
